106484b7369f0770678704d0d31bd04643db707b
issyl0 <isabell@issyl0.co.uk>
2012-04-21 16:22:00 +0100

    Added script that gets the weather station data from the JSON file provided by the MetOffice.

---
+++ script/fetch-weatherstationdata-json-new.rb
@@ -0,0 +1,26 @@
+## Parse the JSON file with the MetOffice data we need in it. ##
+
+require 'rubygems'
+require 'json'
+require 'open-uri'
+require 'mongo'
+
+# Read the file.
+url = "http://partner.metoffice.gov.uk/public/val/wxobs/all/json/sitelist?key=56b621fb-4298-4631-88eb-2c380b914330"
+file = open(url)
+content = file.read()
+# Output the JSON file in its entirety.
+weatherdatafull = JSON.parse(content)
+# puts weatherdatafull['Locations']['Location']
+
+# Database connection + collection (tables).
+dbConnection = Mongo::Connection.new
+db = dbConnection['weathertrumps']
+collection = db['stations']
+
+# Save data to database.
+weatherdatafull['Locations']['Location'].each do|weatherstations|
+  collection.save({"_id" => weatherstations['@id'],
+                   "name" => weatherstations['@name'],
+                   "loc" => [weatherstations['@latitude'].to_f,weatherstations['@longitude'].to_f]})
+end
