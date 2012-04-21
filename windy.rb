2c448bdc7bb104eda5ee144ea82636ef797611b2
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-04-21 17:23:28 +0100

    Add sinatra app and view

--- windy.rb
+++ windy.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'mongo'
+require 'time'
+
+conn     = Mongo::Connection.new
+db       = conn['weathertrumps']
+checkins = db['checkins']
+
+# def get_top(checkins, field, since)
+#   query = Hash.new
+#   query['created_at'] = {"$gt" => since}
+
+#   return checkins
+# end
+
 
 get '/' do
-  erb :windy
+  query = {"created_at" => {"$gt" => Time.now - 86400}}
+  # opts  = {"sort" => ["stats.wind_speed", Mongo::DESCENDING]}
+  results = checkins.find(query) #, opts)
+
+  erb :windy, :locals => {"results" => results}
 end
