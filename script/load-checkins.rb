83d080db5fba70769885accc9fe95613078663aa
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-04-21 16:41:00 +0100

    Fix document format and set username to 'windytrumps'

--- script/load-checkins.rb
+++ script/load-checkins.rb
@@ -7,7 +7,7 @@ db       = conn['weathertrumps']
 checkins = db['checkins']
 stations = db['stations']
 
-def get_station(status)
+def get_station(stations, status)
   stations.find_one({"loc" => {"$near" => [status.geo.latitude, status.geo.longitude]}})
 end
 
@@ -19,20 +19,24 @@ def get_stats(status, station)
 end
 
 
-Twitter.search("to:roryoung", :rpp => 50, :result_type => "recent").map do |status|
+Twitter.search("to:windytrumps", :rpp => 50, :result_type => "recent").map do |status|
   unless status.geo.nil?
-    station = get_station(status)
-    datum   = get_stats(status, station)
-    checkins.save({
-      "_id" => status.id,
-      "username" => status.from_user,
-      "geo" => [status.geo.latitude, status.geo.longitude],
-      "stats" => {
-        "wind_speed" => datum['@S'],
-        "temperature" => datum['@T'],
-        "gust_speed" => datum['@G']
+    puts status.from_user
+    station = get_station(stations, status)
+    unless station.nil?
+      datum   = get_stats(status, station)
+      checkins.save({
+        "_id" => status.id,
+        "username" => status.from_user,
+        "geo" => [status.geo.latitude, status.geo.longitude],
+        "created_at" => status.created_at,
+        "stats" => {
+          "wind_speed" => datum['@S'].to_f,
+          "temperature" => datum['@T'].to_f,
+          "gust_speed" => datum['@G'].to_f
+        }
       }
-    }
-    )
+      )
+    end
   end
 end
